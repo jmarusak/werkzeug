3e686fd907b5f21a3d141c3c5d91a6be68535419
jmarusak <jmarusak@users.noreply.github.com>
2024-04-20 09:35:42 -0400

    using main args for x,y,width,height params

--- src/main/scala/com/maly/pdftool/Crop.scala
+++ src/main/scala/com/maly/pdftool/Crop.scala
@@ -1,5 +1,7 @@
 package com.maly.pdftool
 
+import scala.sys.exit
+
 import org.apache.pdfbox.pdmodel.{PDDocument,PDPage}
 import org.apache.pdfbox.pdmodel.common.PDRectangle
 import java.io.{File,FileInputStream}
@@ -7,25 +9,39 @@ import java.io.{File,FileInputStream}
 object Crop extends App {
   println("pdf crop tool v1.0")
 
-  val inputFile = new File("/home/loco/scala.pdf")
-  val outputFile = new File("/home/loco/scala_cropped.pdf")
+  if (args.length != 5) {
+    println("Usage: scala Crop <filename> <x> <y> <width> <height>")
+    exit(1)
+  }
+
+  val filename = args(0)
+  val x = args(1).toInt
+  val y = args(2).toInt
+  val width = args(3).toInt
+  val height = args(4).toInt
+
+  val inputFile = new File(filename)
+  val outputFile = new File(filename.replaceFirst(".pdf", "_cropped.pdf"))
 
   try {
     val document: PDDocument = PDDocument.load(inputFile)
-    val cropBox: PDRectangle = new PDRectangle(50, 0, 400, 565)
+    val cropBox: PDRectangle = new PDRectangle(x, y, width, height)
 
     val pages = document.getPages
     val iterator = pages.iterator()
     while (iterator.hasNext) {
       val page = iterator.next().asInstanceOf[PDPage]
+      println(page.getCropBox)
       page.setCropBox(cropBox)
-      //println(page.getCropBox)
     }
 
     document.save(outputFile)
     document.close()
 
-    println("Cropping completed.")
+    val ipadRatio = 2048.0 / 1536.0
+    val pdfRatio = (height - y).toDouble / (width - x).toDouble
+
+    println(s"Cropping completed, iPad: $ipadRatio, PDF: $pdfRatio")
   }
   catch {
     case e: Exception => e.printStackTrace()
